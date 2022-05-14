d69970eeb591aa0e5bd461469128dfe743f5636f
Gonzzaa1 <98622541+Gonzzaa1@users.noreply.github.com>
2022-05-13 22:16:24 -0300

    Add files via upload

---
+++ Parcial_1/trabajo.h
@@ -0,0 +1,18 @@
+#include "bicicleta.h"
+#include "servicio.h"
+#include "fecha.h"
+
+#ifndef TRABAJO_H_INCLUDED
+#define TRABAJO_H_INCLUDED
+
+typedef struct
+{
+    int id;
+    int idBicicleta;
+    int idServicio;
+    eFecha fecha;
+}
+
+#endif // TRABAJO_H_INCLUDED
+
+
